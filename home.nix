8b79a775b4b910dc64e9a6120b3a6e46ba6c230b
Judson <nyarly@gmail.com>
2019-01-27 12:56:48 -0800

    Adding direnv config

--- home.nix
+++ home.nix
@@ -2,5 +2,22 @@
 
 {
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+
+    direnv = {
+      enable = true;
+      stdlib = builtins.readFile ./home/config/direnvrc;
+    };
+  };
+
+  xsession = {
+    enable = true;
+    windowManager.command = let
+      xmonad = pkgs.xmonad-with-packages.override {
+        packages = self: [ self.xmonad-contrib self.taffybar ];
+      };
+    in
+      "${xmonad}/bin/xmonad";
+  };
 }
