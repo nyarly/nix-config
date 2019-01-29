ad4232e8556afba2919cd351827b79f504f505c7
Judson <nyarly@gmail.com>
2019-01-29 12:24:46 -0800

    Serious fish config

--- home.nix
+++ home.nix
@@ -9,7 +9,42 @@
       enable = true;
       stdlib = builtins.readFile ./home/config/direnvrc;
     };
+
+    # XXX Add chruby support (chruby module)
+    #
+    fish = let
+      filterDir = f: ds: filter (n: f n ds[n]) (builtins.attrNames ds);
+      configScripts = path: filterDir (path: type: type != "directory" && builtins.match "[.]fish$" path) (builtins.readDir path);
+      configs = path: map (p: builtins.readFile (path + p)) (configScripts path);
+    in
+      {
+        enable = true;
+        shellInit = [
+          "ulimit -n 4096"
+          ''
+          function fish_greeting
+          end
+          ''
+          "__refresh_gpg_agent_info"
+        ];
+        loginShellInit = [
+          "set -g __fish_git_prompt_show_informative_status yes"
+          "set -eg EDITOR # Use set -xU EDITOR and VISUAL"
+          "set -gx PAGER \"less -RF\""
+          "set -gx MANPATH \"\" $MANPATH /run/current-system/sw/share/man"
+          "set -gx RIPGREP_CONFIG_PATH ~/.config/ripgreprc"
+        ] + configs ./home/config/fish/login ;
+        interactiveShellInit = [
+          "stty start undef"
+          "stty stop undef"
+          "stty -ixon"
+          "set -x fish_color_search_match  'normal' '--background=878787'"
+          "bind \e\; 'commandline -r -t (commandline -t | sed \"s/:\(\d*\)/ +\1/\")'"
+        ] + configs ./home/config/fish/interactive ;
+
+      };
   };
+
 
   xsession = {
     enable = true;
---
+++ home/config/fish/login/nix.fish
@@ -0,0 +1,25 @@
+if test -d "$HOME/.nix-profile"
+  set -x NIX_REMOTE daemon
+  set NIX_LINK "$HOME/.nix-profile"
+
+  # Set the default profile.
+  if not [ -L "$NIX_LINK" ];
+    echo "creating $NIX_LINK" >&2
+    set -l _NIX_DEF_LINK /nix/var/nix/profiles/default
+    /run/current-system/sw/bin/ln -s "$_NIX_DEF_LINK" "$NIX_LINK"
+  end
+
+  set PATH $NIX_LINK/bin $NIX_LINK/sbin $PATH
+  set MANPATH $NIX_LINK/share/man $MANPATH
+
+  # Append ~/.nix-defexpr/channels/nixpkgs to $NIX_PATH so that
+  # <nixpkgs> paths work when the user has fetched the Nixpkgs
+  # channel.
+  if test -n $NIX_PATH;
+    set NIX_PATH {$NIX_PATH}:nixpkgs=$HOME/.nix-defexpr/channels/nixpkgs
+  else
+    set NIX_PATH nixpkgs=$HOME/.nix-defexpr/channels/nixpkgs
+  end
+
+  set SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt
+end
