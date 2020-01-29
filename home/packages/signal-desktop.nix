b7310a30478f64653ece64d8d256fa9a969fcfbc
Judson <nyarly@gmail.com>
2020-01-29 10:48:06 -0800

    Updating signal

--- home/packages/signal-desktop.nix
+++ home/packages/signal-desktop.nix
@@ -1,10 +1,10 @@
-{pkgs, fetchurl, ...}:
+{pkgs, fetchurl, lib, ...}:
 pkgs.signal-desktop.overrideAttrs (oldAttrs: rec {
   name = "signal-desktop-${version}";
   pname = "signal-desktop";
-  version = "1.29.3";
+  version = "1.30.0";
   src = fetchurl {
-    url = "https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_1.29.3_amd64.deb";
-    sha256 = "1rkj6rwmwwvyd5041r96j1dxlfbmc6xsdrza43c0ykdrhfj73h11";
+    url = "https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_1.30.0_amd64.deb";
+    sha256 = "1gbvna40sc83s7mwip5281yn4bs0k19fj061y0xzwkvh0yk06x3i"; # lib.fakeSha256;
   };
 })
