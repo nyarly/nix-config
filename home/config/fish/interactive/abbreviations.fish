c3e2b6662e9e873314edd9b4dba84f07e95517f5
Judson Lester <nyarly@gmail.com>
2020-08-12 15:07:56 -0700

    Fish UI

--- home/config/fish/interactive/abbreviations.fish
+++ home/config/fish/interactive/abbreviations.fish
@@ -1,1 +1,10 @@
-abbr -g ta task add
+abbr -a -g -- exg 'exa -lh --git'
+abbr -a -g -- k kubectl
+abbr -a -g -- kn 'kubectl config set-context --current --namespace'
+abbr -a -g -- kr 'kubectl -n rook-ceph'
+abbr -a -g -- ta 'task add'
+abbr -a -g -- v nvim
+abbr -a -g -- view 'nvim -R'
+abbr -a -g -- vs 'nvim -S Session.vim'
+abbr -a -g -- ps procs
+abbr -a -g -- ls exa
--- home/config/fish/interactive/modern_cli_tools.fish
+++ home/config/fish/interactive/modern_cli_tools.fish
@@ -1,4 +1,3 @@
 set -x BAT_THEME ansi-light
 alias ls exa
 alias cat bat
-alias ps procs
