d4722acee4132b2611772410a16321a754b5caa6
Judson Lester <nyarly@gmail.com>
2021-08-12 12:30:01 -0700

    Modernized htop

--- home.nix
+++ home.nix
@@ -162,23 +162,35 @@ in
 
       htop = {
         enable = true;
-        settings = {
-          cpu_count_from_zero = true;
-          delay = 15;
-          fields = [
-            "PID" "USER" "PRIORITY" "NICE"
-            "M_SIZE" "M_RESIDENT" "M_SHARE"
-            "STATE" "PERCENT_CPU" "PERCENT_MEM"
-            "UTIME" "COMM"
+        settings = with config.lib.htop.fields;{
+          ccount_guest_in_cpu_meter=0;
+          color_scheme=0;
+          delay=15;
+          detailed_cpu_time=0;
+          cpu_count_from_zero = false;
+          fields =  [
+            PID USER PRIORITY NICE
+            M_SIZE M_RESIDENT M_SHARE
+            STATE PERCENT_CPU PERCENT_MEM
+            UTIME COMM
           ];
           hide_threads = true;
           hide_userland_threads = true;
-          left_meters = [ "AllCPUs" "Memory" "Swap" ];
-          right_meters = [ "Tasks" "LoadAverage" "Uptime" ];
-          shadow_other_users = true;
+          shadow_other_users = false;
           show_program_path = false;
-          sort_key = "PERCENT_MEM";
-        };
+          sort_key = PERCENT_MEM;
+        } // (with config.lib.htop; leftMeters [
+          (bar "AllCPUs2")
+          (bar "Memory")
+          (bar "Swap")
+          (text "Zram")
+        ]) // (with config.lib.htop; rightMeters [
+          (text "Tasks")
+          (text "LoadAverage")
+          (text "Uptime")
+          (text "Systemd")
+        ]);
+
       };
 
       ssh = let
