6f2596d1e7209db316920aef0bea367243f3a420
Judson Lester <nyarly@gmail.com>
2022-04-03 17:12:33 -0700

    Remove dependence on old .gpg-agent-info

--- home/config/transitional/fish/functions/__refresh_gpg_agent_info.fish
+++ home/config/transitional/fish/functions/__refresh_gpg_agent_info.fish
@@ -2,9 +2,11 @@ function __refresh_gpg_agent_info --description 'Reload ~/.gpg-agent-info'
 	if not test -c $GPG_TTY
     set -x GPG_TTY (tty)
   end
-	cat ~/.gpg-agent-info | sed 's/=/ /' | while read key value
-        #echo $key $$key "->" $value
-  set -e $key
-  set -U -x $key "$value"
-end
+  if set -q $XDG_RUNTIME_DIR
+    set sock "$XDG_RUNTIME_DIR"/gnupg/S.gpg-agent.ssh
+  else
+    set sock /run/user/(id -u)/gnupg/S.gpg-agent.ssh
+  end
+
+  set -g SSH_AUTH_SOCK $sock
 end
